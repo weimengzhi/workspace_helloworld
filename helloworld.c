ca6ddbb2c968bc96c518585a2a203d0cf37f9b09
weimengzhi <weimengzhi@broadmobi.com>
2018-07-10 18:06:57 +0800

    from previous commited rev as new branch, and modify

--- helloworld.c
+++ helloworld.c
@@ -3,5 +3,16 @@
 int main(int argc, char *argv[])
 {
 	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("HELLO WORLD\n");
+	printf("br WORLD\n");
 	return 0;
 }
