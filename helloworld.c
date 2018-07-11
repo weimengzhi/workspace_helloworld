4d52a688c97dbdde2e96e458eae1dfd613dba1ae
weimengzhi <weimengzhi@broadmobi.com>
2018-07-11 15:39:15 +0800

    jijdijediejicduchdic jijdijediejicduchdic

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,11 @@ int main(int argc, char *argv[])
 {
 	printf("HELLO WORLD\n");
 	printf("something in dev branch");
+	printf("something in dev branch");
+	printf("something in dev branch");
+	printf("something in dev branch");
+	printf("something in dev branch");
+	printf("something in dev branch");
 
 	printf("something new 17:39in dev branch");
 	printf("something new 17:56in dev branch");
