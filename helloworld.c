cf22b7377f31ce1735ce6a519635543d60e7f259
weimengzhi <weimengzhi@broadmobi.com>
2018-07-11 15:49:15 +0800

    jdfiejdfeidjeidjei

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+	int i = 0;
 	printf("HELLO WORLD\n");
-//	printf("something in dev branch");
+	printf("something in dev branch");
+	for(i = 0; i < 16; i++)
+	{
+		
+		printf("something in dev branch");
+	}
+
 	return 0;
 }
